f3bad45e0db71acd82b42922634cace28b7f948d
Khoirul-Hamdillah <91958224+Khoirul-Hamdillah@users.noreply.github.com>
2023-04-22 19:54:18 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("localhost","root","root","LoginSystem");
+    $con = mysqli_connect("sql201.epizy.com","epiz_31883038","uAkLScjovZ","epiz_31883038_data_user");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
